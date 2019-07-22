3e844378f5a9ba4862d29dba4f02769b8aacaada
yangyuqing123 <1453582072@qq.com>
2019-07-22 16:41:25 +0800

    角色编码和角色名称有必输校验，且编码必须以字母开头，只能输入字母，数字，_，-，/，角色名称提交时去除首位空白

--- react/src/app/demo/containers/role/form.js
+++ react/src/app/demo/containers/role/form.js
@@ -4,20 +4,17 @@ import { observer } from 'mobx-react';
 import Store2 from './stores/index2';
 import Menu from './menu';
 import './style/form.less';
+const FormItem = Form.Item;
 const Option = Select.Option;
+function hasErrors(fieldsError) {
+    return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
+@Form.create()
 @observer
 class FormField extends Component{
-    constructor(props){
-        super(props);
-        this.state={
-            code:"",
-            name:"",
-        }
-    }
     componentDidMount() {
         this.loadData();
     }
-    
     loadData = () => {
         Store2.loadData();
     }
@@ -28,54 +25,88 @@ class FormField extends Component{
         });
         return options;
     }
-    // validateName=(e)=>{
-    //     let name=e.target.value.toString();
-    //     const re = /^[A-Za-z].{2,20}$/;
-    //     console.log(name);
-    //     if(!re.test(name)){
-    //         alert("名称必须以字母开头，只能输入字母，数字，_，-，/");
-    //     }
-        
-       
-    // }
-    // validateCode=(e)=>{
-    //     const re2=/^[0-9a-zA-Z\_\-\/\s]+$/;
-    //     let code=e.target.value.toString();
-    //     if(!re2.test(name)){
-    //         alert("编码必须以字母开头，只能输入字母，数字，_，-，/");
-    //     }
-       
-    // }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+              values.roleName=values.roleName.trimStart();
+            console.log('Received values of form: ', values);
+          }
+        });
+    }
     render(){
+        console.log("renders");
+        const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+        // Only show error after a field is touched.
+        const roleNameError = isFieldTouched('roleName') && getFieldError('roleName');
+        const roleCodeError = isFieldTouched('roleCode') && getFieldError('roleCode');
         return(
             <div>
-                <div style={{marginLeft:"15px"}}>
-                 <Row gutter={8}>
-                    <Col span={6}>
-                    <Input  onChange={this.validateName} placeholder="角色名称" required label="Basic" />
-                    </Col>
-                    
-                    <Col span={6}>
-                    <Input  onChange={this.validateCode} placeholder="项目编码" required label="Basic" />
-                    </Col>
-                </Row> 
+                <Form onSubmit={this.handleSubmit}>
+                <div>
+                    <div style={{marginLeft:"15px"}}>  
+                        <Row gutter={8}>
+                            <Col span={6}>
+                            <FormItem
+                validateStatus={roleNameError ? 'error' : ''}
+                help={roleNameError || ''}
+                >
+                {getFieldDecorator('roleName', {
+                    rules: [{ 
+                        required: true,
+                        message: '请输入名称!',
+                    }]
+                })(
+                    <Input type="text" placeholder="角色名称" />
+                )}
+                </FormItem>
+                            </Col>
+                            <Col span={6}>
+                            <FormItem
+                            validateStatus={roleCodeError ? 'error' : ''}
+                            help={roleCodeError || ''}
+                            >
+                            {getFieldDecorator('roleCode', {
+                                rules: [{ 
+                                    required: true, 
+                                    message: '不匹配!',
+                                    pattern:/^[0-9a-zA-Z\_\-\/]+$/}],
+                            })(
+                                <Input type="text" placeholder="角色编码" />
+                            )}
+                            </FormItem>
+                            </Col>
+                        </Row> 
+                    </div>
+                    <Row>
+                        <Col span={24}>
+                            <Select mode="角色标签" label="角色标签" style={{width:"30vw"}} placeholder="角色标签" value="" onChange={this.handleCurrencyChange}>
+                            {this.renderSelect()}
+                            </Select>
+                        </Col>
+                    </Row> 
+                    <Row>
+                        <Col span={24}>
+                            <Menu/>
+                        </Col>
+                    </Row>  
+                    <Row>
+                        <Col span={6}>
+                            <FormItem>
+                                <Button
+                                    type="primary"
+                                    htmlType="submit"
+                                    disabled={hasErrors(getFieldsError())}
+                                >
+                                    Log in
+                                </Button>
+                            </FormItem>
+                        </Col>
+                    </Row> 
                 </div>
-                <Row>
-                    <Col span={24}>
-                        <Select mode="角色标签" label="角色标签" style={{width:"30vw"}} placeholder="角色标签" value="" onChange={this.handleCurrencyChange}>
-                           {this.renderSelect()}
-                        </Select>
-                    </Col>
-                </Row> 
-                <Row>
-                    <Col span={24}>
-                        <Menu/>
-                    </Col>
-                </Row>   
+            </Form>
             </div>
-            
         )
-        
     }
 }
 
